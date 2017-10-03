d560bb1dc1a1cd68e6eb559c3b60ab436f506c19
Andrew Hand <31429165+ExNilos@users.noreply.github.com>
2017-10-03 17:14:14 +1100

    Started work on the power manager and the bomb power.

---
+++ Assets/Scripts/PowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+// This script is attached to every players car. It contains functions for activating each ability.
+public class PowerManager : MonoBehaviour
+{
+    public XboxController controller;
+    public 
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
