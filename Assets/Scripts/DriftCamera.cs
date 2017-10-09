901f380ab294ae997a71efb4528e7173db3a7b62
Andrew Hand <31429165+ExNilos@users.noreply.github.com>
2017-10-09 16:59:36 +1100

    Didn't get much done beyond revision 32/33. I need to fix the jitteriness, add snap back (where the camera returns to normal after a bit), add clamping, and fix the view switching.

--- Assets/Scripts/DriftCamera.cs
+++ Assets/Scripts/DriftCamera.cs
@@ -50,20 +50,12 @@ public class DriftCamera : MonoBehaviour
     public float ctrlY = 0.0f;
     public float finalInputX = 0.0f;
     public float finalInputY = 0.0f;
-    public float clampAngle = 80.0f;
-    public float inputSensitivity = 150.0f;
+    public float maxClampX = 150.0f;
+    public float minClampX = -150.0f;
+    public float maxClampY = 35.0f;
+    public float minClampY = -20.0f;
+    public float inputSensitivity = 2.0f;
     public float smoothing = 6f; // How fast the camera lerps
-
-    // Smoothly transitions from normal view to centre view. The smoothing causes the camera to lag behind the player though.
-    public void ViewCentre()
-    {
-        // rotates cameraPivot to look at the centre of the map.
-        cameraFocus.transform.LookAt(mapCentre);
-
-        // rotates and moves the camera. Smoothly lerps between the cameras current transform to the desired transform.
-        transform.position = Vector3.Lerp(transform.position, cameraDesiredPosition.position, Time.deltaTime * smoothing);
-        transform.rotation = Quaternion.Slerp(transform.rotation, cameraDesiredPosition.rotation, Time.deltaTime * smoothing);
-    }
 
     // Smoothly transitions from centre view to normal view. The smoothing causes the camera to lag behind the player though.
     public void ViewFront()
@@ -75,6 +67,17 @@ public class DriftCamera : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, cameraDesiredPosition.position, Time.deltaTime * smoothing);
         transform.rotation = Quaternion.Slerp(transform.rotation, cameraDesiredPosition.rotation, Time.deltaTime * smoothing);
     }
+    // Smoothly transitions from normal view to centre view. The smoothing causes the camera to lag behind the player though.
+    public void ViewCentre()
+    {
+        // rotates cameraPivot to look at the centre of the map.
+        cameraFocus.transform.LookAt(mapCentre);
+
+        // rotates and moves the camera. Smoothly lerps between the cameras current transform to the desired transform.
+        transform.position = Vector3.Lerp(transform.position, cameraDesiredPosition.position, Time.deltaTime * smoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, cameraDesiredPosition.rotation, Time.deltaTime * smoothing);
+    }
+
 
     void Start()
     {
@@ -93,23 +96,24 @@ public class DriftCamera : MonoBehaviour
         mouseY = Input.GetAxis("Mouse Y");
         ctrlX = XCI.GetAxis(XboxAxis.RightStickX);
         ctrlY = XCI.GetAxis(XboxAxis.RightStickY);
-        finalInputX = mouseX + ctrlX;
-        finalInputY = mouseY + ctrlY;
-        //Debug.Log("\nfinalInputX: ");
-        //Debug.Log(finalInputX);
-        // Debug.Log("\nfinalInputY: ");
-        //Debug.Log(finalInputY);
+        finalInputX = (mouseX + ctrlX) * inputSensitivity;
+        finalInputY = (mouseY + ctrlY) * inputSensitivity;
+        /*Debug.Log("\nfinalInputX: ");
+        Debug.Log(finalInputX);
+        Debug.Log("\nfinalInputY: ");
+        Debug.Log(finalInputY);
+        */
 
-        //if (Input.GetKeyDown(advancedOptions.switchRightViewKey))
-        //    m_ShowingRightSideView = !m_ShowingRightSideView;
+
+
         if (Input.GetKeyDown(advancedOptions.switchCentreViewKey))
             showingCentreView = !showingCentreView;
 
         if (XCI.GetButtonDown(XboxButton.Y, controller))
             showingCentreView = !showingCentreView;
-
-        if (advancedOptions.updateCameraInUpdate)
-            UpdateCamera();
+        UpdateCamera();
+        //if (advancedOptions.updateCameraInUpdate)
+        //    UpdateCamera();
     }
 
     private void LateUpdate()
@@ -122,19 +126,23 @@ public class DriftCamera : MonoBehaviour
     {
 
         // Allows swapping between different camera views.
-        if (showingCentreView)
+        /*if (showingCentreView)
         {
             ViewCentre();
         }
         else
         {
             ViewFront();
-        }
+        }*/
 
-       
+        // Preserves orientation.
         transform.LookAt(cameraPivot);
-        cameraPivot.Rotate(-finalInputY, finalInputX, 0);
         
+        cameraPivot.Rotate(-finalInputY, finalInputX, 0);
+
+        transform.position = Vector3.Lerp(transform.position, cameraDesiredPosition.position, Time.deltaTime * smoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, cameraDesiredPosition.rotation, Time.deltaTime * smoothing);
+
 
         //Quaternion targetRotation = Quaternion.Euler(currentY, currentX, 0);
 
