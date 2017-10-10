b0bdb3e676ce0ed339dd0603e3a69e244e28a2e2
Andrew Hand <31429165+ExNilos@users.noreply.github.com>
2017-10-11 10:43:37 +1100

    testing CameraController.cs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform pivot;
+    public Vector3 offset = new Vector3(0, 1, 0);
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        pivot = GameObject.FindGameObjectWithTag("Player").transform.GetChild(5).transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+
+		
+	}
+}
