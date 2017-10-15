72d8d810d1c804dfc97c8d711ff61e373bac666d
JoelGoodchild <joel.goodchild@hotmail.com>
2017-10-16 10:56:34 +1100

    Level manager and timer script alpha build

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float myTimer = 180.0f;
+    public Text timerText;
+    public int roundCounter = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTimer -= Time.deltaTime;
+        timerText.text = myTimer.ToString("f2");
+        print(myTimer);
+        if (myTimer <= 0.05f)
+        {
+            SceneManager.LoadScene(0);
+            ++roundCounter;
+        }
+	}
+}
