5018d7abfb4b1a56c4113ecdfaed03b3c9bc996d
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2017-10-17 12:26:21 +1100

    Fixed and tested Score.cs, fully working, noticed that the Damage.cs for the bumper v bumper detection doesn't work, added a comment as well as a function BodyScore() wich gets the damage the player deal and carry's it to the Score.cs script so it can be displayed

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -30,15 +30,33 @@ public class Damage : MonoBehaviour
 
     }
 
+    private void BodyScore()
+    {
+        if (rigidBody.transform.parent.name == "Car1")
+            Score.Player1ScoreActual += (int)damageTaken;
+        if (rigidBody.transform.parent.name == "Car2")
+            Score.Player2ScoreActual += (int)damageTaken;
+        if (rigidBody.transform.parent.name == "Car3")
+            Score.Player3ScoreActual += (int)damageTaken;
+        if (rigidBody.transform.parent.name == "Car4")
+            Score.Player4ScoreActual += (int)damageTaken;
+    }
+
+    private void BumperScore()
+    {
+
+    }
 
     void OnCollisionEnter(Collision other)
     {
         if (carVelo >= minAttackVelocity)
         {
+            //The Bumper v Bumper Doesnt trigger - Ryan
             //Calculation occurs with head on collision with another car
             if (other.gameObject.tag == "Front Bumper")
             {
                 other.gameObject.GetComponent<CarController>().carHealth -= (damageTaken / 2);
+                //Gets a copy of the damage dealt for the Score.CS NOT NOT DELETE
                 Debug.Log("bumper hit");
                 Debug.Log(transform.parent.GetComponent<CarController>().playerID + ":  " + other.gameObject.GetComponent<CarController>().carHealth);
                 return;
@@ -48,6 +66,8 @@ public class Damage : MonoBehaviour
             else if (other.gameObject.tag == "Player")
             {
                 other.gameObject.GetComponent<CarController>().carHealth -= damageTaken;
+                //Gets a copy of the damage dealt for the Score.CS NOT NOT DELETE
+                BodyScore();
                 Debug.Log(transform.parent.GetComponent<CarController>().playerID + ":  " + other.gameObject.GetComponent<CarController>().carHealth);
                 return;
             }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
-    public static int Player1ScoreActual;
-    public static int Player2ScoreActual;
-    public static int Player3ScoreActual;
-    public static int Player4ScoreActual;
+    public static int Player1ScoreActual = 0;
+    public static int Player2ScoreActual = 0;
+    public static int Player3ScoreActual = 0;
+    public static int Player4ScoreActual = 0;
     
     public Text Player1Score;
     public Text Player2Score;
@@ -20,21 +20,21 @@ public class Score : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (Player1Score.gameObject.tag == "PlayerScore 1")
+        if (Player1Score.gameObject.name == "PlayerScore1")
         {
-            Player1Score.text = "Player 1:" + Player1ScoreActual.ToString();
+            Player1Score.text = "Player 1: " + Player1ScoreActual.ToString();
         }
-        if (Player2Score.gameObject.tag == "PlayerScore 2")
+        if (Player2Score.gameObject.name == "PlayerScore2")
         {
-            Player2Score.text = "Player 2:" + Player2ScoreActual.ToString();
+            Player2Score.text = "Player 2: " + Player2ScoreActual.ToString();
         }
-        if (Player3Score.gameObject.tag == "PlayerScore 3")
+        if (Player3Score.gameObject.name == "PlayerScore3")
         {
-            Player3Score.text = "Player 3:" + Player3ScoreActual.ToString();
+            Player3Score.text = "Player 3: " + Player3ScoreActual.ToString();
         }
-        if (Player4Score.gameObject.tag == "PlayerScore 4")
+        if (Player4Score.gameObject.name == "PlayerScore4")
         {
-            Player4Score.text = "Player 4:" + Player4ScoreActual.ToString();
+            Player4Score.text = "Player 4: " + Player4ScoreActual.ToString();
         }
     }
 }
