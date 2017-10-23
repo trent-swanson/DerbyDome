d1af06fb2c8ec6c4c0ac06924db628bbf22250a9
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2017-10-23 13:45:32 +1100

    Car Flip added

---
+++ Assets/Scripts/CarFlip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarFlip : MonoBehaviour
+{
+    RaycastHit hit;
+    float Distance;
+    public float timer;
+    public GameObject Player;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("RayCast");
+    }
+
+    private void FlipCheck()
+    {
+        float CurrentRotation = Player.transform.parent.rotation.z;
+        Vector3 Up = transform.TransformDirection(Vector3.up);
+        if (Physics.Raycast(transform.position, Up, out hit))
+        {
+            timer += Time.deltaTime;
+            Distance = hit.distance;
+            if (hit.collider.gameObject.tag == "Ground")
+            {
+                if (timer >= 3)
+                {
+                    Player.transform.parent.parent.rotation = Quaternion.identity;
+                    Player.transform.rotation = Quaternion.identity; 
+                    timer = 0;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Checks and flips the player if they are upside down
+        FlipCheck();
+    }
+}
